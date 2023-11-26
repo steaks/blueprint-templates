14ad535c63d927d2609afca17987711527f82fb5
Steven Wexler <steven.m.wexler@gmail.com>
2023-11-26 00:17:18 -0700

    Update script

--- createBlueprint.sh
+++ createBlueprint.sh
@@ -5,6 +5,11 @@ if [[ $# -eq 0 ]] ; then
     exit 0
 fi
 
+if [[ $1 = "{your-app-name}" ]] ; then
+    echo 'Provide a name for your app'
+    exit 0
+fi
+
 wget https://raw.githubusercontent.com/steaks/blueprint-templates/main/helloworld-blueprint.tar.gz
 tar -xzf helloworld-blueprint.tar.gz
 mv helloworld $1
